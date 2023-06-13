96ee574a70e11acac7382d4ff9eaced023b36ad9
John Loyd Mulit <jlmulit68@gmail.com>
2023-06-14 07:39:31 +0800

    fix audios and thumbnails not deleted when all data is dropped

--- src/main/utils/dropStories.ts
+++ src/main/utils/dropStories.ts
@@ -2,6 +2,20 @@ import fs from 'fs'
 import os from 'os'
 
 export function dropStories() {
-    const mainPath = `${os.homedir()}/Music/audiobook-data/data`
-    fs.writeFileSync(`${mainPath}/data.json`, JSON.stringify({ stories: [] }))
+    const mainPath = `${os.homedir()}/Music/audiobook-data`
+
+    // drop all data
+    fs.writeFileSync(`${mainPath}/data/data.json`, JSON.stringify({ stories: [] }))
+
+    // drop all audio
+    const audioFiles = fs.readdirSync(`${mainPath}/audios`, { encoding: 'utf8' })
+    audioFiles.forEach(audio => {
+        fs.rmSync(`${mainPath}/audios/${audio}`)
+    })
+
+    // drop all thumbnails
+    const thumbnailFiles = fs.readdirSync(`${mainPath}/thumbnails`, { encoding: 'utf8' })
+    thumbnailFiles.forEach(thumbnail => {
+        fs.rmSync(`${mainPath}/thumbnails/${thumbnail}`)
+    })
 }
